021753f66497ab30fbcaef23957fb6ed259e0db7
imac <leman.mammadly23@gmail.com>
2023-06-27 16:32:04 +0400

    sidebar responsive

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.querySelectorAll(".tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.querySelectorAll(".tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.querySelectorAll(".tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.querySelectorAll(".tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
-window.addEventListener('load', function () {
-    openTab(event, 'tab1');
-  });
-
+window.addEventListener("load", function () {
+  openTab(event, "tab1");
+});
 
 //   let tabcontent1 = document.querySelector("#tab1");
 //  tabcontent1.style.display = 'block'
+
+$(document).ready(function () {
+  $(".menu-bar-icon").click(function () {
+    $(".menu-mobile-sidebar").animate({ left: "0" }, 500);
+  });
+
+  $(".x-logo").click(function () {
+    $(".menu-mobile-sidebar").animate({ left: "-45vh" }, 500);
+  });
+
+});
